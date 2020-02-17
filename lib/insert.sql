a93e7b3cd8903a9627c87853a6aafd531cf78373
Ian Marrs <ian.t.marrs@gmail.com>
2020-02-17 19:34:26 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,8 +1,8 @@
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("NULL", NULL, "NULL", "NULL", "NULL", true)
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("NULL", NULL, "NULL", "NULL", "NULL", true)
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("NULL", NULL, "NULL", "NULL", "NULL", true)
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("NULL", NULL, "NULL", "NULL", "NULL", true)
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("NULL", NULL, "NULL", "NULL", "NULL", true)
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("NULL", NULL, "NULL", "NULL", "NULL", true)
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("NULL", NULL, "NULL", "NULL", "NULL", true)
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("NULL", NULL, "NULL", "NULL", "NULL", true)
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("NULL", NULL, "NULL", "NULL", "NULL", true);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("NULL", NULL, "NULL", "NULL", "NULL", true);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("NULL", NULL, "NULL", "NULL", "NULL", true);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("NULL", NULL, "NULL", "NULL", "NULL", true);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("NULL", NULL, "NULL", "NULL", "NULL", true);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("NULL", NULL, "NULL", "NULL", "NULL", true);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("NULL", NULL, "NULL", "NULL", "NULL", true);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("NULL", NULL, "NULL", "NULL", "NULL", true);
