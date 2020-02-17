819fce5af06176dc4ff6a24ddd4013d9ef2dbdc5
Ian Marrs <ian.t.marrs@gmail.com>
2020-02-17 19:35:46 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -5,4 +5,4 @@ INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("NULL",
 INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("NULL", NULL, "NULL", "NULL", "NULL", 1);
 INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("NULL", NULL, "NULL", "NULL", "NULL", 1);
 INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("NULL", NULL, "NULL", "NULL", "NULL", 1);
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("NULL", NULL, "NULL", "NULL", "NULL", 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES (NULL, NULL, "NULL", "NULL", "NULL", 1);
