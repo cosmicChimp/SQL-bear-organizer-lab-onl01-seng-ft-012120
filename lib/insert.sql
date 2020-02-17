719f328b97da0f39d6a0042cda699665faffa130
Ian Marrs <ian.t.marrs@gmail.com>
2020-02-17 19:41:09 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,8 +1,8 @@
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("NULL", NULL, "NULL", "NULL", "NULL", 1);
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("NULL", NULL, "NULL", "NULL", "NULL", 1);
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("NULL", NULL, "NULL", "NULL", "NULL", 1);
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("NULL", NULL, "NULL", "NULL", "NULL", 1);
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("NULL", NULL, "NULL", "NULL", "NULL", 1);
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("NULL", NULL, "NULL", "NULL", "NULL", 1);
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("NULL", NULL, "NULL", "NULL", "NULL", 1);
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES (NULL, NULL, "NULL", "NULL", "NULL", 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Oscar", 4, "Male", "Brown", "Timid", 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Johnny", 5, "Male", "Black", "Aggressive", 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Celia", 2, "Female", "Brown", "Passive", 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Frank", 2, "Male", "Black", "Aggressive", 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Gatsby", 3, "Male", "Black", "Aggressive", 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Ursa Major", 5, "Female", "Brown", "Lazy", 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Top Hat", 4, "Male", "Brown", "Timid", 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES (NULL, 1, "Female", "Tan", "Friendly", 1);
